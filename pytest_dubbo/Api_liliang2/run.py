cebca99825db41519c7ffe26353fcc52129073ba
liliangyuzhou <1287641566@qq.com>
2019-06-02 18:39:03 +0800

    test code

---
+++ pytest_dubbo/Api_liliang2/run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : run.py
+# @Author: LILIANG
+# @Date  : 2019/5/29
+# @Desc  :  test
+import sys
+sys.path.append('./')#目录加到project根目录地址
+print(sys.path)
+import unittest
+from Api_liliang2.common import HTMLTestRunnerNew
+from Api_liliang2.common import constants
+
+
+
+discover=unittest.defaultTestLoader.discover(constants.cases_file,"test_*.py")
+with open(constants.report_file + '/report.html','wb+') as file:
+    runner=HTMLTestRunnerNew.HTMLTestRunner(stream=file,
+                                         verbosity=2,
+                                         title="测试报告",
+                                         description="tester",
+                                         tester="李亮")
+    runner.run(discover)
+
+
